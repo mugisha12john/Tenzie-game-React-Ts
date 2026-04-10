9fdbcd28b9fae82f80e01ccd63c3ebb5c6fff6d0
Jean Baptiste MUGISHA <johnbutista004@gmail.com>
2026-04-10 15:51:28 +0200

    feat add tailwind and implement header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <main className="bg-white w-4/5 mx-auto rounded-2xl">
+        <div className="flex justify-center mt-10 flex-col items-center">
+          <h1 className="text-3xl  font-bold mt-5">Tenzies</h1>
+          <p className="text-lg w-64 text-center mt-4">
+            Roll until all dice are the same. Click each die to freeze it at its
+            current value between rolls.
+          </p>
+        </div>
+      </main>
     </>
   );
 }
