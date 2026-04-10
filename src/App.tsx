7f7ee362952e90ecac461049b49a0702c634e2ac
Jean Baptiste MUGISHA <johnbutista004@gmail.com>
2026-04-10 16:38:00 +0200

    feat: add roll button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <main className="bg-white max-w-3xl mx-auto rounded-2xl h-screen">
+      <main className="bg-white max-w-3xl mx-auto flex justify-center flex-col items-center rounded-2xl mt-10">
         <div className="flex justify-center mt-2 flex-col items-center">
           <h1 className="text-3xl  font-bold mt-5">Tenzies</h1>
           <p className="text-lg w-64 text-center mt-4">
@@ -10,18 +10,41 @@ function App() {
           </p>
         </div>
 
-        <div className="grid grid-cols-5  gap-4 place-content-center place-items-center">
-         <button className="p-2 font-bold text-2xl border border-black w-30">1</button>
-         <button className="p-2 font-bold text-2xl border border-black w-30">2</button>
-         <button className="p-2 font-bold text-2xl border border-black w-30">3</button>
-         <button className="p-2 font-bold text-2xl border border-black w-30">4</button>
-         <button className="p-2 font-bold text-2xl border border-black w-30">5</button>
-         <button className="p-2 font-bold text-2xl border border-black w-30">6</button>
-         <button className="p-2 font-bold text-2xl border border-black w-30">7</button>
-         <button className="p-2 font-bold text-2xl border border-black w-30">8</button>
-         <button className="p-2 font-bold text-2xl border border-black w-30">9</button>
-       <button className="p-2 font-bold text-2xl border border-black w-30">10</button>
+        <div className="grid grid-cols-5  gap-2 w-4/5  mx-auto mt-10">
+          <button className="p-2 font-bold text-2xl border border-black w-30">
+            1
+          </button>
+          <button className="p-2 font-bold text-2xl border border-black w-30">
+            2
+          </button>
+          <button className="p-2 font-bold text-2xl border border-black w-30">
+            3
+          </button>
+          <button className="p-2 font-bold text-2xl border border-black w-30">
+            4
+          </button>
+          <button className="p-2 font-bold text-2xl border border-black w-30">
+            5
+          </button>
+          <button className="p-2 font-bold text-2xl border border-black w-30">
+            6
+          </button>
+          <button className="p-2 font-bold text-2xl border border-black w-30">
+            7
+          </button>
+          <button className="p-2 font-bold text-2xl border border-black w-30">
+            8
+          </button>
+          <button className="p-2 font-bold text-2xl border border-black w-30">
+            9
+          </button>
+          <button className="p-2 font-bold text-2xl border border-black w-30">
+            10
+          </button>
         </div>
+        <button className="bg-indigo-600 font-bold m-20 text-xl p-2 w-40 ">
+          Roll
+        </button>
       </main>
     </>
   );
